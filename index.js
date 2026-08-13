9de70b1379c152b679562f360fa5d93fa7e3f960
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-08-13 23:32:29 +0600

    reviews collection database added

--- index.js
+++ index.js
@@ -28,9 +28,16 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
     const menuCollection = client.db("bistro_boss").collection("menu");
+    const reviewsCollection = client.db("bistro_boss").collection("reviews");
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
+      res.send(result);
+    });
+
+
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewsCollection.find().toArray();
       res.send(result);
     });
 
