d70ff709a4e55ff37849fa3d0e0128362d31421a
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-06 07:47:07 +0600

    cart query with parameter

--- index.js
+++ index.js
@@ -48,7 +48,9 @@ async function run() {
     // cart collection api
 
     app.get('/carts', async (req, res) => {
-      const result = await cartsCollection.find().toArray();
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await cartsCollection.find(query).toArray();
       res.send(result);
     });
     
