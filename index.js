31459dd8479eacda48988b358b9a0771912ae428
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-12 22:57:16 +0600

    users collection

--- index.js
+++ index.js
@@ -30,9 +30,19 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+    const userCollection = client.db("bistro_boss").collection("users");
     const menuCollection = client.db("bistro_boss").collection("menu");
     const reviewsCollection = client.db("bistro_boss").collection("reviews");
     const cartsCollection = client.db("bistro_boss").collection("carts");
+
+
+    // users collection api
+
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const result = await userCollection.insertOne(user);
+      res.send(result);
+    });
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
