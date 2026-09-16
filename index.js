6405b45308d2c1fb0119a8f971a41711a7b8afec
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-16 22:13:35 +0600

    user patch

--- index.js
+++ index.js
@@ -55,6 +55,17 @@ async function run() {
       res.send(result);
     });
 
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        }
+      };
+      const result = await userCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
     app.get('/menu', async (req, res) => {
       const result = await menuCollection.find().toArray();
