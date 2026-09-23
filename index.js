401f031b83bf12d99db47d018372979b85df3ff0
Shoyebreza <68115608+shoyebreza@users.noreply.github.com>
2026-09-23 23:48:32 +0600

    api menu add

--- index.js
+++ index.js
@@ -134,6 +134,18 @@ async function run() {
       res.send(result);
     });
 
+    app.post('/menu', verifyToken, verifyAdmin, async (req, res) => {
+      const newItem = req.body;
+      const result = await menuCollection.insertOne(newItem);
+      res.send(result);
+    });
+
+    app.delete('/menu/:id', verifyToken, verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result);
+    });
 
     app.get('/reviews', async (req, res) => {
       const result = await reviewsCollection.find().toArray();
